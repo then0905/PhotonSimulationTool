846e582dc2847eb616e7836e314a05eb3e54d19b
hiraeth0905 <then0905@gmail.com>
2026-02-07 23:32:13 +0800

    [Fix] 修正 道具buff效果要移除的縮排錯誤 應該在for迴圈外(原本會是第一筆資料進行還原後就刪除剩下的沒有被還原)

--- battle_simulator.py
+++ battle_simulator.py
@@ -119,8 +119,8 @@ class BattleCharacter:
                 for op in itemData.ItemEffectDataList:
                     self.SkillEffectStatusOperation(op.InfluenceStatus, (op.AddType == "Rate"),
                                                     -1 * op.EffectValue * stack)
-                    self.buff_skill.pop(buff_item_id, None)
-                    self.buff_bar.remove_effect(buff_item_id)
+                self.buff_skill.pop(buff_item_id, None)
+                self.buff_bar.remove_effect(buff_item_id)
         #負面狀態遞減
         for debuff_id in list(self.debuff_skill):
             op, debuffDuration = self.debuff_skill[debuff_id]
